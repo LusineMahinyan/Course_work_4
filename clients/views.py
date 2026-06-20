0f404ee7103e0b3ee03948611b11a482a5b63f69
Лусине <lumahinyan@mail.ru>
2026-06-20 17:50:06 +0300

    Clients

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Client
 
-# Create your views here.
+
+class ClientListView(ListView):
+    model = Client
+    template_name = "clients/client_list.html"
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ["email", "full_name", "comment"]
+    success_url = reverse_lazy("client_list")
+    template_name = "clients/client_form.html"
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ["email", "full_name", "comment"]
+    success_url = reverse_lazy("client_list")
+    template_name = "clients/client_form.html"
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = reverse_lazy("client_list")
+    template_name = "clients/client_confirm_delete.html"
