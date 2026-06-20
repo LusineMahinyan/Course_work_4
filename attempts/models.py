9162f7c17af109f43df87cffa5b1343de274e2c3
Лусине <lumahinyan@mail.ru>
2026-06-20 17:38:33 +0300

    Миграции

--- attempts/models.py
+++ attempts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Attempt(models.Model):
+    STATUS_CHOICES = [
+        ("success", "Успешно"),
+        ("failed", "Не успешно"),
+    ]
+
+    attempt_time = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    server_response = models.TextField()
+
+    mailing = models.ForeignKey('mailings.Mailing', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.status} - {self.attempt_time}"
+
--- mailings/models.py
+++ mailings/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from clients.models import Client
 
-# Create your models here.
+
+class Message(models.Model):
+    subject = models.CharField(max_length=255)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.subject
+
+
+class Mailing(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    recipients = models.ManyToManyField(Client)
+
+    def get_status(self):
+        now = timezone.now()
+
+        if now < self.start_time:
+            return "Создана"
+        elif self.start_time <= now <= self.end_time:
+            return "Запущена"
+        return "Завершена"
+
+    def __str__(self):
+        return f"Mailing {self.id}"
